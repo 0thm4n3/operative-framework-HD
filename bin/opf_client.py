85bfd8253fd628a5bb4e1d9c59b12b6650063479
Tristan Granier <graniet75@gmail.com>
2018-06-18 12:34:16 +0200

    update: binary class

--- bin/opf_client.py
+++ bin/opf_client.py
@@ -5,35 +5,44 @@ import datetime
 import requests
 import sys
 import os
-sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)).rsplit('/', 1)[0])
 from colorama import Fore, Style
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)).rsplit('/', 1)[0])
 from framework import config
 
 
-def print_log(color_string, type_string, text):
-    return datetime.datetime.today().strftime('[%Y-%m-%d %H:%M:%S] ') + color_string + str(type_string) +  Style.RESET_ALL + " " + str(text)
+class OperativeBinary(object):
 
+    def __init__(self):
+        self.meta = {
+            'author': 'Tristan Granier',
+            'description': 'launch operative framework HD client.'
+        }
+        self.run()
 
-def main():
-    print print_log(Fore.BLUE, 'INFO', 'Welcome to Operative Framework client' + str(config.OPERATIVE_FRAMEWORK_VERSION))
-    try:
-        requests.head("http://127.0.0.1:" + config.BACKEND_PORT + "/")
-    except:
-        print print_log(Fore.RED, 'ERROR', "Please run backend sudo python bin/opf_server.py")
-        sys.exit()
-    if not os.path.isdir(os.path.dirname(os.path.realpath(__file__)).rsplit('/', 1)[0] + "/client"):
-        print print_log(Fore.RED, 'ERROR', "directory : '" + os.path.dirname(os.path.realpath(__file__)).rsplit('/', 1)[0] + "/client' not found")
-        sys.exit()
-    print print_log(Fore.BLUE, 'INFO', 'Starting client in background....')
-    print print_log(Fore.GREEN, 'SUCCESS', 'framework start  at 127.0.0.1:' + config.FRONTEND_PORT)
-    cmd = "npm start --prefix " + os.path.dirname(os.path.realpath(__file__)).rsplit('/', 1)[0] + "/client/"
-    a = subprocess.Popen(cmd, shell=True)
-    stdout, stderr = a.communicate()
-    if a.returncode != 0:
-        print print_log(Fore.RED, "ERROR", "A error has been occurred with a client")
-        print print_log(Fore.RED, "ERROR", "Please run it manually: npm run build --prefix 'client/'")
-        sys.exit()
+    @staticmethod
+    def print_log(color_string, type_string, text):
+        return datetime.datetime.today().strftime('[%Y-%m-%d %H:%M:%S] ') + color_string + str(type_string) +  Style.RESET_ALL + " " + str(text)
+
+    def run(self):
+        print self.print_log(Fore.BLUE, 'INFO', 'Welcome to Operative Framework client' + str(config.OPERATIVE_FRAMEWORK_VERSION))
+        try:
+            requests.head("http://127.0.0.1:" + config.BACKEND_PORT + "/")
+        except:
+            print self.print_log(Fore.RED, 'ERROR', "Please run backend sudo python bin/opf_server.py")
+            sys.exit()
+        if not os.path.isdir(os.path.dirname(os.path.realpath(__file__)).rsplit('/', 1)[0] + "/client"):
+            print self.print_log(Fore.RED, 'ERROR', "directory : '" + os.path.dirname(os.path.realpath(__file__)).rsplit('/', 1)[0] + "/client' not found")
+            sys.exit()
+        print self.print_log(Fore.BLUE, 'INFO', 'Starting client in background....')
+        print self.print_log(Fore.GREEN, 'SUCCESS', 'framework start  at 127.0.0.1:' + config.FRONTEND_PORT)
+        cmd = "npm start --prefix " + os.path.dirname(os.path.realpath(__file__)).rsplit('/', 1)[0] + "/client/"
+        a = subprocess.Popen(cmd, shell=True)
+        stdout, stderr = a.communicate()
+        if a.returncode != 0:
+            print self.print_log(Fore.RED, "ERROR", "A error has been occurred with a client")
+            print self.print_log(Fore.RED, "ERROR", "Please run it manually: npm run build --prefix 'client/'")
+            sys.exit()
 
 
 if __name__ == "__main__":
-    main()
+    OperativeBinary()
--- bin/opf_server.py
+++ bin/opf_server.py
@@ -4,30 +4,40 @@ import subprocess
 import datetime
 import sys
 import os
-sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)).rsplit('/', 1)[0])
 from colorama import Fore, Style
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)).rsplit('/', 1)[0])
 from framework import config
 
 
-def print_log(color_string, type_string, text):
-    return datetime.datetime.today().strftime('[%Y-%m-%d %H:%M:%S] ') + color_string + str(type_string) +  Style.RESET_ALL + " " + str(text)
+class OperativeBinary(object):
 
+    def __init__(self):
+        self.meta = {
+            'author': 'Tristan Granier',
+            'description': 'launch operative framework HD flask application server.'
+        }
+        self.run()
 
-def main():
-    print print_log(Fore.BLUE, 'INFO', 'Welcome to Operative Framework backend ' + str(config.OPERATIVE_FRAMEWORK_VERSION))
-    if not os.path.isdir(os.path.dirname(os.path.realpath(__file__)).rsplit('/', 1)[0] + "/framework"):
-        print print_log(Fore.RED, 'ERROR', "directory : '" + os.path.dirname(os.path.realpath(__file__)).rsplit('/', 1)[0] + "/framework' not found")
-        sys.exit()
-    print print_log(Fore.BLUE, 'INFO', 'Starting framework in background....')
-    print print_log(Fore.GREEN, 'SUCCESS', 'framework start  at 127.0.0.1:' + config.BACKEND_PORT)
-    cmd = "python " + os.path.dirname(os.path.realpath(__file__)).rsplit('/', 1)[0] + "/framework/app.py"
-    a = subprocess.Popen(cmd, shell=True)
-    stdout, stderr = a.communicate()
-    if a.returncode != 0:
-        print print_log(Fore.RED, "ERROR", "A error has been occurred with a framework")
-        print print_log(Fore.RED, "ERROR", "Please run it manually: sudo python framework/app.py")
-        sys.exit()
+    @staticmethod
+    def print_log(color_string, type_string, text):
+        return datetime.datetime.today().strftime('[%Y-%m-%d %H:%M:%S] ') + color_string + str(type_string) +  Style.RESET_ALL + " " + str(text)
+
+    def run(self):
+        print self.print_log(Fore.BLUE, 'INFO', 'Welcome to Operative Framework backend ' + str(config.OPERATIVE_FRAMEWORK_VERSION))
+        if not os.path.isdir(os.path.dirname(os.path.realpath(__file__)).rsplit('/', 1)[0] + "/framework"):
+            print self.print_log(Fore.RED, 'ERROR', "directory : '" + os.path.dirname(os.path.realpath(__file__)).rsplit('/', 1)[0] + "/framework' not found")
+            sys.exit()
+        print self.print_log(Fore.BLUE, 'INFO', 'Starting framework in background....')
+        print self.print_log(Fore.GREEN, 'SUCCESS', 'framework start  at 127.0.0.1:' + config.BACKEND_PORT)
+        cmd = "python " + os.path.dirname(os.path.realpath(__file__)).rsplit('/', 1)[0] + "/framework/app.py"
+        a = subprocess.Popen(cmd, shell=True)
+        stdout, stderr = a.communicate()
+        if a.returncode != 0:
+            print self.print_log(Fore.RED, "ERROR", "A error has been occurred with a framework")
+            print self.print_log(Fore.RED, "ERROR", "Please run it manually: sudo python framework/app.py")
+            sys.exit()
+        return True
 
 
 if __name__ == "__main__":
-    main()
+    OperativeBinary()
